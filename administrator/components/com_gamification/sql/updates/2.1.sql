36b9bab450260710c1e0c10108fb338a29e05ea2
Todor Iliev <tosho.iliev@gmail.com>
2016-05-24 17:59:13 +0300

    added table achievements

--- administrator/components/com_gamification/sql/updates/2.1.sql
+++ administrator/components/com_gamification/sql/updates/2.1.sql
@@ -11,3 +11,15 @@ CREATE TABLE IF NOT EXISTS `#__gfy_rewards` (
   `group_id` smallint(5) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__gfy_achievements` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(64) NOT NULL DEFAULT '',
+  `note` varchar(255) DEFAULT NULL,
+  `published` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `params` varchar(2048) NOT NULL DEFAULT '{}',
+  `group_id` smallint(5) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
